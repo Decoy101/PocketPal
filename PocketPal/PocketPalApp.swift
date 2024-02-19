d67b5f678922d86a918009f465e9a23e2056fa2d
Decoy101 <00amangpt00@gmail.com>
2024-02-19 17:15:10 +0530

    Initial Commit

---
+++ PocketPal/PocketPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketPalApp.swift
+//  PocketPal
+//
+//  Created by Aman Gupta on 19/02/24.
+//
+
+import SwiftUI
+
+@main
+struct PocketPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
